5f758f576e102e79e53a727ab77fd9acc39cf511
Alex Braithwaite <abraith93@gmail.com>
2018-01-29 21:28:10 +0000

    Environment working

--- gym_asset_allocation/envs/asset_allocation_env.py
+++ gym_asset_allocation/envs/asset_allocation_env.py
@@ -7,17 +7,28 @@ import pandas as pd
 import quandl
 
 class AssetAllocationEnv(gym.Env):
-    # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # #
     # Overridden methods
-    # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # #
     def __init__(self):
         # prices is a T x n array
         # first indexed by time, then by asset
         self._pull_price_data()
-        
+        n = len(self.asset_price_dfs)
+
+        self.observation_space = spaces.Box(0, np.finfo('d').max, shape=(n,5))
+        self.action_space = spaces.Box(0, 1, shape=(n))
+
+        self._seed()
         self._reset()
 
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
     def _step(self, action):
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+
         self.t += 1
         self._take_action(action)
 
@@ -30,18 +41,20 @@ class AssetAllocationEnv(gym.Env):
 
     def _reset(self):
 
+        # self.price is a dict, keys 'Open','High','Low','Close','Volume'
         self.price = self._get_price_data()
 
-        self.T = self.price.shape[0] # final time
-        self.n = self.price.shape[1] # number of assets
+        self.T = self.price['Close'].shape[0] # final time
+        self.n = self.price['Close'].shape[1] # number of assets
 
-        self.allocation = np.zeros((self.T,self.n)) # fund allocation at time t
-        self.holdings = np.zeros((self.T,self.n)) # actual holdings at time t
+        # These are all for the START of day t
+        self.allocation = np.zeros((self.T, self.n)) # fund allocation
+        self.holdings = np.zeros((self.T, self.n)) # asset holdings
         self.cash = np.zeros((self.T))
         self.cash[0] = 100
 
-        self.value = np.zeros((self.T)) # total value at time t
-        self.value[0] = np.dot(self.holdings[0], self.price[0]) + self.cash[0]
+        self.value = np.zeros((self.T)) # total value
+        self.value[0] = np.dot(self.holdings[0], self.price['Open'][0]) + self.cash[0]
 
         self.t = 0 # current time
 
@@ -50,78 +63,54 @@ class AssetAllocationEnv(gym.Env):
         # TODO
         pass
 
-    # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # #
     # Helper methods
-    # # # # # # # # # # # # # # # # # # # # 
+    # # # # # # # # # # # # # # # # # # # #
     def _pull_price_data(self):
-        assets = ['TSE/9994','TSE/3484']
+        assets = ['TSE/9994', 'TSE/3443']
         self.asset_price_dfs = []
         for asset in assets:
             self.asset_price_dfs += [quandl.get(asset)]
 
     def _get_price_data(self, days=252):
-        year_asset_prices = []
+        days = 20
+        # get slices of length days from each dataframe
+        asset_df_slices = []
         for asset_df in self.asset_price_dfs:
-            i_start = np.random.randint(0,len(asset_df)-days)
-            year_asset_prices += [asset_df.Close[i_start:i_start+days]]
-        asset_prices = np.column_stack(year_asset_prices)
-        return asset_prices
+            if len(asset_df) > days:
+                i_start = self.np_random.randint(0, len(asset_df)-days)
+                asset_df_slice = asset_df.iloc[i_start:i_start+days]
+                asset_df_slices += [asset_df_slice]
+        
+        # stack these together in to prices
+        prices = {}
+        for col in self.asset_price_dfs[0].columns:
+            prices[col] = np.column_stack([df[col] for df in asset_df_slices])
+
+        return prices
 
     def _take_action(self, action):
+        # Normalise action
         if action.sum() > 1:
             action /= action.sum()
+
         # update current allocation, value and holdings
         self.allocation[self.t] = action
 
         # keep holdings[t-1] from t-1 to t
-        self.value[self.t] = np.dot(self.holdings[self.t-1], self.price[self.t]) + self.cash[self.t-1]
-        # TODO add transaction costs; need to think about how to deal with these
-        # simple approach is to cut portfolio value by x%
-        self.holdings[self.t] = np.floor(self.value[self.t] * self.allocation[self.t] / self.price[self.t])
-        self.cash[self.t] = self.value[self.t] - np.dot(self.holdings[self.t], self.price[self.t])
+        self.value[self.t] = np.dot(self.holdings[self.t-1], self.price['Open'][self.t]) + \
+            self.cash[self.t-1]
+
+        # calculate our new holdings
+        self.holdings[self.t] = self.value[self.t] * self.allocation[self.t] / \
+            self.price['Open'][self.t]
+        self.cash[self.t] = (1-action.sum()) * self.value[self.t]
 
     def _get_observation(self):
-        return self.price[self.t]
-    
+        return np.array([self.price[col][self.t] for col in self.price]).transpose()
+
     def _get_reward(self):
         return (self.value[self.t] - self.value[self.t-1]) / self.value[self.t-1]
 
     def _get_over(self):
         return self.t >= self.T
-'''
-data = np.round(np.random.random(size=(10,3))*5) + 10
-aa = AssetAllocation(data)
-
-a1 = np.array([.3,.4,.2])
-a2 = np.array([.4,.2,.1])
-a3 = np.array([.4,.2,.1])
-
-print(data)
-
-print("time = ",aa.t)
-print("\tPrices    =",aa.price[aa.t])
-print("\tValue     =",aa.value[aa.t])
-print("\tAllocation=",aa.allocation[aa.t])
-print("\tHoldings  =",aa.holdings[aa.t])
-print("\tCash      =",aa.cash[aa.t])
-aa.step(a1)
-print("time = ",aa.t)
-print("\tPrices    =",aa.price[aa.t])
-print("\tValue     =",aa.value[aa.t])
-print("\tAllocation=",aa.allocation[aa.t])
-print("\tHoldings  =",aa.holdings[aa.t])
-print("\tCash      =",aa.cash[aa.t])
-aa.step(a2)
-print("time = ",aa.t)
-print("\tPrices    =",aa.price[aa.t])
-print("\tValue     =",aa.value[aa.t])
-print("\tAllocation=",aa.allocation[aa.t])
-print("\tHoldings  =",aa.holdings[aa.t])
-print("\tCash      =",aa.cash[aa.t])
-aa.step(a3)
-print("time = ",aa.t)
-print("\tPrices    =",aa.price[aa.t])
-print("\tValue     =",aa.value[aa.t])
-print("\tAllocation=",aa.allocation[aa.t])
-print("\tHoldings  =",aa.holdings[aa.t])
-print("\tCash      =",aa.cash[aa.t])'''
